6cb323777fc40c80f998e95bee15a6c8e94500f6
Ed Jefferson <e.jefferson@gmail.com>
2024-04-04 14:53:09 +0100

    bundle update

--- public/script.js
+++ public/script.js
@@ -32,7 +32,28 @@ fetch("lad.json")
       shadowSize: [41, 41]
     });
 
+    const greenIcon = new L.Icon({
+      iconUrl:
+        "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
+      shadowUrl:
+        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41]
+    });
+
+    const blueCircle = new L.Icon({
+      iconUrl:
+        "location.svg",
+          iconSize: [25, 25],
+
+      shadowSize: [41, 41]
+    });
+
 const markVisited = (id) => {
+
+  markers[id].setIcon(greenIcon)
   fetch('update_mews', {
     method: 'POST',
     headers: {
@@ -42,6 +63,8 @@ const markVisited = (id) => {
     body: JSON.stringify({id: id, b: 'Textual content'})
   });
 }
+
+let markers = {}
 fetch("all_mews.json")
     .then((response) => 
       response.json()
@@ -50,11 +73,24 @@ fetch("all_mews.json")
       data.mewses.forEach((d) => {
         if (d.boroughs.includes("Kensington And Chelsea")) {
           popupContents  = `<p>${d.name} (${d.id})</p><p><a onClick="markVisited('${d.id}')">Unvisited</a>`
-          L.marker([d.lat, d.lng], {title: d.name + " (" + d.id + ")", icon: redIcon}).addTo(map).bindPopup(popupContents);
+          markers[d.id] = L.marker([d.lat, d.lng], {title: d.name + " (" + d.id + ")", icon: redIcon}).addTo(map).bindPopup(popupContents);
 
         } else {
-          L.marker([d.lat, d.lng], {title: d.name}).addTo(map).bindPopup(d.name);
+          markers[d.id] = L.marker([d.lat, d.lng], {title: d.name}).addTo(map).bindPopup(d.name);
         }
      })}
    
     )
+
+const findMe = () => {
+
+  navigator.geolocation.getCurrentPosition((position) => {
+   //doSomething(position.coords.latitude, position.coords.longitude);
+    L.marker([position.coords.latitude,position.coords.longitude], {icon: blueCircle}).addTo(map)
+
+    map.panTo(new L.LatLng(position.coords.latitude,position.coords.longitude));
+  });
+  
+
+}
+document.getElementById("findme").addEventListener("click",findMe)
