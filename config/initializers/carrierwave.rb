7f6728b044ad5f9c52a74ee6ab1b722e6a511ee7
Ed Jefferson <e.jefferson@gmail.com>
2024-04-06 10:31:33 +0100

    ftp

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,11 @@
+CarrierWave.configure do |config|
+  config.cache_storage = :file
+  config.sftp_host = ENV["FTP_HOST"]
+  config.sftp_user = ENV["FTP_USER"]
+  config.sftp_folder = ENV["FTP_FOLDER"]
+  config.sftp_url = ENV["FTP_URL"]
+  config.sftp_options = {
+    :password => ENV["FTP_PASSWD"],
+    :port     => ENV["FTP_PORT"]
+  }
+end
