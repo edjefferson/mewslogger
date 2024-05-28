a3a196bb7fde4db94a7c8d444d356ff5014802d9
Ed Jefferson <e.jefferson@gmail.com>
2024-05-28 12:05:37 +0100

    news

---
+++ config/initializers/leaflet.rb
@@ -0,0 +1,5 @@
+Leaflet.tile_layer = "https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+# You can also use any other tile layer here if you don't want to use Cloudmade - see http://leafletjs.com/reference.html#tilelayer for more
+Leaflet.attribution = "Your attribution statement"
+Leaflet.max_zoom = 19
+
